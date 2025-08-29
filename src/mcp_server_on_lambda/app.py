a5d7ecd3a485295fb2d9cf2451081b7e72447d28
ajisaka.seiya <ajisaka.seiya@synergy101.jp>
2025-08-29 14:13:06 +0900

    With FastAPI

--- src/mcp_server_on_lambda/app.py
+++ src/mcp_server_on_lambda/app.py
@@ -1,6 +1,8 @@
 import logging
+from contextlib import asynccontextmanager
 
 import uvicorn
+from fastapi import FastAPI
 from fastmcp import FastMCP
 from pydantic import BaseModel
 
@@ -21,6 +23,34 @@ mcp_app = mcp.http_app(
 )
 
 
+# https://gofastmcp.com/integrations/fastapi#combining-lifespans
+@asynccontextmanager
+async def app_lifespan(app: FastAPI):  # type: ignore
+    logger.info("Starting up the app...")
+    yield
+    logger.info("Shutting down the app...")
+
+
+@asynccontextmanager
+async def combined_lifespan(app: FastAPI):  # type: ignore
+    async with app_lifespan(app):
+        async with mcp_app.lifespan(app):  # type: ignore
+            yield
+
+
+app = FastAPI(lifespan=combined_lifespan)
+
+
+@app.get("/")
+def app_root() -> dict:
+    return {"message": "Hello World"}
+
+
+@app.get("/answer")
+def app_answer() -> int:
+    return 42
+
+
 @mcp.tool
 def tool_add(x: int, y: int) -> CalculationResult:
     return CalculationResult(value=x + y)
@@ -31,8 +61,11 @@ def tool_sub(x: int, y: int) -> CalculationResult:
     return CalculationResult(value=x - y)
 
 
+app.mount("/mcp", mcp_app)
+
+
 def main() -> None:
-    uvicorn.run(mcp_app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
 
 
 if __name__ == "__main__":
